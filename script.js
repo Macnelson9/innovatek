03f662358f4f33bb66ba12f272e63da60f9aa124
Macnelson9 <michaelofatu@gmail.com>
2025-03-27 23:25:55 +0100

    Add initial project files with HTML structure, CSS styles, and JavaScript setup

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+document.querySelector('.box');
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     gsap.registerPlugin(Flip, ScrollTrigger, ScrollToPlugin, TextPlugin);
+    
+//     gsap.to(".box", {
+//         rotation: 360,
+//         duration: 2,
+//         scrollTrigger: {
+//             trigger: '.box',
+//             markers: true,
+//             scrub: true
+//         }
+//     })
+// })
+
+// document.addEventListener('DOMContentLoaded', function () {
+//  Map feature
+//     const map = L.map('map').setView([37.7749, -122.4194], 13);
+
+//     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//     }).addTo(map);
+
+//     L.marker([37.7749, -122.4194]).addTo(map)
+//     .bindPopup('Innovatek lives Here!')
+//     .openPopup();
+// })
