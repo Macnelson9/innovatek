43eb6dcec882643740fd9cdd320d1637221816eb
Macnelson9 <michaelofatu@gmail.com>
2025-04-03 23:09:49 +0100

    Add map feature with OpenStreetMap integration; update styles for team members, testimonials, and blog sections

--- script.js
+++ script.js
@@ -13,6 +13,19 @@ closeBtn.addEventListener('click', function () {
     mobileNav.classList.remove('active');
 })
 
+document.addEventListener('DOMContentLoaded', function () {
+    //  Map feature
+        const map = L.map('map').setView([37.7749, -122.4194], 13);
+    
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+    
+        L.marker([37.7749, -122.4194]).addTo(map)
+        .bindPopup('Innovatek lives Here!')
+        .openPopup();
+    })
+
 // document.addEventListener('DOMContentLoaded', function () {
 //     gsap.registerPlugin(Flip, ScrollTrigger, ScrollToPlugin, TextPlugin);
     
@@ -24,17 +37,4 @@ closeBtn.addEventListener('click', function () {
 //             scrub: true
 //         }
 //     })
-// })
-
-// document.addEventListener('DOMContentLoaded', function () {
-//  Map feature
-//     const map = L.map('map').setView([37.7749, -122.4194], 13);
-
-//     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//     }).addTo(map);
-
-//     L.marker([37.7749, -122.4194]).addTo(map)
-//     .bindPopup('Innovatek lives Here!')
-//     .openPopup();
 // })
